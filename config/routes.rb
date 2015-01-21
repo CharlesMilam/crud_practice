9461eaf5c519caa0c827f140cc6636bafbfba650
charles milam <charles.milam@gmail.com>
2015-01-20 21:33:32 -0600

    Add route to create new video.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Metube::Application.routes.draw do
   root to: "videos#index"
   get "/videos/new", to: "videos#new"
+  post "/videos" => "videos#create"
   get "/videos", to: "videos#index"
   get "/videos/:id", to: "videos#show"
 
