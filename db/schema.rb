c516679c5da95b3d80779502410e2f4a74f62c7e
charles milam <charles.milam@gmail.com>
2015-01-21 19:29:51 -0600

    Auto modified after migration run of comments table creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121133151) do
+ActiveRecord::Schema.define(version: 20150121220447) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "video_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sounds", force: true do |t|
     t.string   "title"
