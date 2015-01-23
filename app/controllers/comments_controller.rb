941cb06cb749a26f372856d2f1434d3a938bbed5
charles milam <charles.milam@gmail.com>
2015-01-22 23:52:17 -0600

    Add load_media method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,31 @@
 class CommentsController < ApplicationController
-  def new
-    @comment = Comment.new
+  before_filter :load_media
+
+  # def new
+  #   @video = Video.new
+  #   @comment = Comment.new
+  # end
+  def show
+    @comment = Comment.find(params[:id])
   end
 
   def create
-    comment = Comment.new(comment_params)
+    #@media = Video.find(params[:video_id])
+    #@comment = @media.comments.create(comment_params)
+    comment = Comment.new
+    comment.content = params[:comment][:content]
+    comment.media = @media
     comment.save
-    redirect_to "/videos/#{comment_params[:video_id]}"
+    redirect_to :back
   end
 
   private
   def comment_params
     params.require(:comment).permit(:video_id, :content)
+  end
+
+  def load_media
+    resource, id = request.path.split('/')[1,2]
+    @media = resource.singularize.classify.constantize.find(id)
   end
 end
